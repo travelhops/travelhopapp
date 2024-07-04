ee6055921e32f4b8606b7a09a55bdeb0a0a9489b
rudrasama <luckybairagi96@gmail.com>
2024-07-05 00:42:23 +0530

    commit

--- src/Components/admin/StarSelection.jsx
+++ src/Components/admin/StarSelection.jsx
@@ -1,50 +1,79 @@
 import react from 'react';
+import {useEffect, forwardRef, useImperativeHandle} from 'react';
 
+const StarSelection = (props, ref)=>{
 
-const StarSelection = ()=>{
+    
+    let stars = 0;
 
+    const setStars = (input, inputs)=>{
+        const index = parseInt(input.getAttribute("data-index"))-1;
 
-    const allInputs = document.querySelector(".starContainer input[type='checkbox']");
+        for(let i = 0; i <= index; i++){
+            inputs[i].checked = true;
+        }
+        
+        for(let i = index+1; i < inputs.length; i++){
+            inputs[i].checked = false;
+        }
 
-    const setStars = (input)=>{
-        console.log(input);
+        stars = index+1;
+
     }
 
-    Array.from(allInputs).forEach((input)=>{
-        input.addEventListener("click", (event)=>{
-            setStars(event.target);
-        })
-    });
+
+    const getStars = ()=>{
+        return stars;
+    }
+
+    useImperativeHandle(ref, () => ({
+        getStars
+    }));
+
+    
+
+
+
+
+    useEffect(()=>{
+        const inputs = document.querySelectorAll(".starContainer input");
+        Array.from(inputs).forEach((input)=>{
+            input.addEventListener("click", (event)=>{
+                setStars(event.target, inputs);
+                console.log(getStars());
+            });
+        });
+    },[]);
 
     return (
-        <div className="starContainer">
+        <div className="starContainer" ref={ref}>
             <input type="checkbox" id="star1" data-index="1" />
             <label htmlFor="star1">
-                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star" aria-hidden></i>
             </label>
 
             <input type="checkbox" id="star2" data-index="2" />
             <label htmlFor="star2">
-                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star" aria-hidden></i>
             </label>
 
             <input type="checkbox" id="star3" data-index="3" />
             <label htmlFor="star3">
-                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star" aria-hidden></i>
             </label>
 
             <input type="checkbox" id="star4" data-index="4" />
             <label htmlFor="star4">
-                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star" aria-hidden></i>
             </label>
 
             <input type="checkbox" id="star5" data-index="5" />
             <label htmlFor="star5">
-                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star" aria-hidden></i>
             </label>
         </div>
     );
 }
 
 
-export default StarSelection;
+export default forwardRef(StarSelection);
--- src/pages/admin/CreateTestimonials.jsx
+++ src/pages/admin/CreateTestimonials.jsx
@@ -1,16 +1,28 @@
 import react from 'react';
+import {useEffect, useRef} from 'react';
 import StarSelection from '../../Components/admin/StarSelection';
 import AdminPanel from '../../Components/admin/AdminPanel';
 
 const CreateTestimonials = ()=>{
+    const starSelection = useRef(null);
+
+    useEffect(()=>{
+    },[]);
+
+    const getStars = ()=>{
+        console.log(starSelection.current.getStars());
+    }
+
     return (
         <div className="w-full flex">
             <div className="flex-[0_0_20%]">
                 <AdminPanel/>
             </div>
             <div className="flex-[0_0_80%] bg-gray-100 py-12">
-                <StarSelection/>
+                <StarSelection ref={starSelection}/>
             </div>
+
+            <button onClick={getStars}>Get</button>
 
         </div>
  
